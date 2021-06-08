13f49f02f73f82199428a396647c890bfeeba659
saul-h <saulhernandez217@gmail.com>
2021-06-07 21:25:15 -0700

    Welcome not displaying username

--- app/src/main/java/com/example/assignment1/Welcome.java
+++ app/src/main/java/com/example/assignment1/Welcome.java
@@ -2,7 +2,9 @@ package com.example.assignment1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Welcome extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Intent intent = getIntent();
+        String username = intent.getStringExtra(Login.USERNAME);
+
+        TextView textView = findViewById(R.id.txtWelcome);
+        String welcomeMessage = "Welcome " + username + "!";
+        textView.setText(welcomeMessage);
     }
 }
